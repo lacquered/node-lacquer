b78ac990ba9f6ee3d7b587ef3033c2dff067cdfa
John Kim <yohany@gmail.com>
2015-07-20 00:29:22 +0900

    제주도에는 문어피자가 있네... 정말 배불러 움직이지도 못하겠는데 자꾸 입에 음식이 들어간다....

--- routes/reserve.js
+++ routes/reserve.js
@@ -3,13 +3,31 @@ var router = express.Router();
 var mongoPersister = require('../lib/persister/mongoPersister');
 
 router.post('/write', function (req, res, next) {
-
   mongoPersister.createReservation(req.body, function (err, result) {
-    console.log("SUCCESS !!!! : " + result);
-    res.json({ user: 'tobi' });
+    res.json(result);
   })
+});
 
+router.post('/', function (req, res, next) {
+  mongoPersister.retrieveReservation(req.body, function (err, result) {
+    res.json(result);
+  })
+});
 
+router.post('/remove', function (req, res, next) {
+  mongoPersister.removeReservation(req.body, function (err, result) {
+    if(err) console.error(err);
+    console.log(result);
+    res.json(result);
+  })
+});
+
+router.post('/update', function (req, res, next) {
+  mongoPersister.updateReservation(req.body, function (err, result) {
+
+    if(err) console.error(err);
+    res.json(result);
+  })
 });
 
 module.exports = router;
