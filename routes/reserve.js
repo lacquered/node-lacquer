be48c4be82c9c79847644b6693631735646ed3f0
John Kim <yohany@gmail.com>
2015-07-19 13:54:57 +0900

    마라도에서 사진도 찍고... 이제 돌아가는 배 기다리는 중. 인증샷도 찍었네 :)

--- routes/reserve.js
+++ routes/reserve.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mongoDatabase = require('../lib/persister/mongoDatabase');
+var mongoPersister = require('../lib/persister/mongoPersister');
 
-router.post('/write', function(req, res, next) {
+mongoDatabase.config('', 'lacquer', function (err, message) {
+    if (!err) {
+      console.info('  - Mongodb is connected');
+    }
+    console.log(message);
+  }
+);
+
+
+router.post('/write', function (req, res, next) {
   //console.log(req.body);
 
   console.log(req.body.name);
+
+  mongoPersister.createReservation(req.body, function (err, result) {
+    console.log("SUCCESS !!!! : " + result);
+  })
 
   res.send('respond with a resource');
 });
