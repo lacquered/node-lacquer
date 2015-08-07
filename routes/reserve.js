5c098331c17de5e2c35db1f326a85cd4ce231359
John Kim <yohany@gmail.com>
2015-08-07 23:30:56 +0900

    ^^;

--- routes/reserve.js
+++ routes/reserve.js
@@ -4,7 +4,7 @@ var mongoPersister = require('../lib/persister/mongoPersister');
 
 
 var listSource = ["통영", "가배", "저구", "대포"];
-var listTime = ["09:30","15:30"];
+var listTime = ["09:30", "15:30"];
 
 
 // 예약 등록 FORM
@@ -20,73 +20,96 @@ router.post('/new', function (req, res, next) {
 
   mongoPersister.createReservation(req.body, function (err, result) {
     console.log(err, result);
-    if(err == 'EXISTED') {
+    if (err == 'EXISTED') {
       res.json({
         status: err,
         result: result
       })
-    }else{
+    } else {
       res.json({
         status: 'OK',
         result: result
       })
     }
 
-  })
+  });
 });
 
 
 // 예약 수정 FORM
 router.get('/update', function (req, res, next) {
 
-  console.log('asdfasdf');
-  console.log(req.query.d);
-
   var param = {
     listSource: listSource,
     listTime: listTime
   };
 
-  if(req.query.d) {
+  if (req.query.d) {
     var tmpParam = JSON.parse(req.query.d);
 
     console.log(tmpParam);
     param['pSource'] = tmpParam.source;
-    param['pDate'] = tmpParam.date;
+    param['pDate'] = tmpParam.date.substr(0,4)+'/'+tmpParam.date.substr(4,2)+'/'+tmpParam.date.substr(6,2)+'/';
     param['pTime'] = tmpParam.time;
     param['pTel1'] = tmpParam.tel1;
     param['pTel2'] = tmpParam.tel2;
     param['pTel3'] = tmpParam.tel3;
   }
-
-
-  console.log(param);
-
   res.render('page_reservation_update', param);
 });
 
-
-
-router.post('/', function (req, res, next) {
+router.post('/retrieve', function (req, res, next) {
   mongoPersister.retrieveReservation(req.body, function (err, result) {
-    res.json(result);
-  })
+
+
+    console.log(err, result);
+
+    if (err == 'NOTEXISTED') {
+      res.json({
+        status: 'NOTEXISTED',
+        result: result
+      })
+    } else {
+      res.json({
+        status: 'OK',
+        result: result
+      })
+    }
+
+  });
 });
 
-router.post('/remove', function (req, res, next) {
+router.post('/delete', function (req, res, next) {
   mongoPersister.removeReservation(req.body, function (err, result) {
-    if(err) console.error(err);
-    console.log(result);
-    res.json(result);
-  })
+    if (err) {
+      res.json({
+        status: 'ERROR',
+        result: err
+      })
+    } else {
+      res.json({
+        status: 'OK',
+        result: result
+      })
+    }
+  });
 });
 
 router.post('/update', function (req, res, next) {
   mongoPersister.updateReservation(req.body, function (err, result) {
 
-    if(err) console.error(err);
-    res.json(result);
-  })
+    if (err) {
+      res.json({
+        status: 'ERROR',
+        result: err
+      })
+    } else {
+      res.json({
+        status: 'OK',
+        result: result
+      })
+    }
+  });
 });
 
 module.exports = router;
