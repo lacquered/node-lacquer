dff576c4e8782a368175ec2cfd1dba057e96915d
John Kim <yohany@gmail.com>
2015-08-10 10:33:54 +0900

    문구수정

--- add-account.js
+++ add-account.js
@@ -3,8 +3,8 @@ var Account = require('./lib/admin/account');
 require('./lib/persister/mongoDatabase').config('10.0.0.4:27017', 'lacquer', function (err, message) {
     if (!err) {
 
-      var _username = 'admin1@lacquer.com';
-      var _password = '1234';
+      var _username = 'clubes';
+      var _password = 'admin1234';
 
       Account.register(new Account({ username : _username }), _password, function(err, account) {
         if (err) {
